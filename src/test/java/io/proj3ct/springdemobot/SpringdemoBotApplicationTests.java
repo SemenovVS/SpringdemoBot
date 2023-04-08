cbae72aa42e13a3729c0a150257e25a137a1962f
Semenov Volodymyr <thesemenovv@gmail.com>
2023-04-08 17:28:01 +0300

    .

---
+++ src/test/java/io/proj3ct/springdemobot/SpringdemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.springdemobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
